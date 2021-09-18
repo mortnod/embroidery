2ae4ba4af18e795861022bd5ad4c19385879b22c
Morten Noddeland <morten@noddeland.no>
2021-09-19 00:44:12 +0200

    Set Vercel region to Sweden

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,6 @@ module.exports = {
   extends: [
     'airbnb',
     'airbnb/hooks',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'airbnb-typescript',
     'plugin:@next/next/core-web-vitals',
     'prettier',
