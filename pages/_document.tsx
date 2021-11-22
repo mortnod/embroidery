ae70905b52bcec0d51073c22775d1edc8e214c45
Morten Noddeland <morten@noddeland.no>
2021-11-23 00:46:14 +0100

    Add comment regarding server-side rendering Stitches

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,7 @@ class MyDocument extends Document {
           <style
             id="stitches"
             // eslint-disable-next-line react/no-danger
-            dangerouslySetInnerHTML={{ __html: getCssText() }}
+            dangerouslySetInnerHTML={{ __html: getCssText() }} // Configure server side rendering for Stitches https://stitches.dev/docs/server-side-rendering
           />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
